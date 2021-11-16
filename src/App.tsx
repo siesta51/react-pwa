ea7728c53549b657be32209a3d12c2855f5101f3
siesta51 <h.sotobayashi@gmail.com>
2021-11-16 17:30:33 +0900

    code fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function notifyMe() {
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var notification = new Notification("Hi there!");
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        var notification = new Notification("Hi there!");
+      }
+    });
+  }
+
+  // At last, if the user has denied notifications, and you
+  // want to be respectful there is no need to bother them any more.
+}
 
 function App() {
   return (
@@ -18,6 +44,7 @@ function App() {
         >
           Learn React
         </a>
+        <div onClick={() => notifyMe()}>通知許可</div>
       </header>
     </div>
   );
