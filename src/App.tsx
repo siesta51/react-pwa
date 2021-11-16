120ddec784aaea4d60f99e9eb1ddbe5a9b17a9f0
siesta51 <h.sotobayashi@gmail.com>
2021-11-16 17:59:14 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function notifyMe() {
   // Let's check whether notification permissions have already been granted
   else if (Notification.permission === "granted") {
     // If it's okay let's create a notification
-    var notification = new Notification("Hi there!");
+    new Notification("Hi there!");
   }
 
   // Otherwise, we need to ask the user for permission
@@ -20,13 +20,21 @@ function notifyMe() {
     Notification.requestPermission().then(function (permission) {
       // If the user accepts, let's create a notification
       if (permission === "granted") {
-        var notification = new Notification("Hi there!");
+        new Notification("Hi there!");
       }
     });
   }
 
   // At last, if the user has denied notifications, and you
   // want to be respectful there is no need to bother them any more.
+}
+
+function sendMessage() {
+  if (Notification.permission === "granted") {
+    navigator.serviceWorker.ready.then((registration: any) => {
+      registration.active.postMessage("hello!!!");
+    });
+  }
 }
 
 function App() {
@@ -46,6 +54,7 @@ function App() {
           Learn React
         </a>
         <div onClick={notifyMe}>通知許可</div>
+        <div onClick={sendMessage}>通知送信</div>
       </header>
     </div>
   );
